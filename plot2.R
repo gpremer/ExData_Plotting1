0c8dabcb3465e9e916a3acceef3d2d96f01dace1
Geert Premereur <Geert.Premereur@gmail.com>
2015-01-11 18:16:50 +0100

    Extracted date time calculation

--- plot2.R
+++ plot2.R
@@ -1,15 +1,14 @@
 library(dplyr)
+
+### 
+### Since the data file is large, it is not uploaded to github. The script expects that you extract the zip in it's working directory. 
+###
+
 power <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
 power_t <- tbl_df(power)
 
-# POSIXlt not allowed in mutate
-#mutate(power_t,Date2=strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))
-
-
-% power_feb <- filter(power,as.Date(Date,"%d/%m/%Y") %in% c(as.Date(Date,"1/2/2007"),as.Date("2/2/2007","%d/%m/%Y"))) 
 power_feb <- filter(power,Date %in% c("1/2/2007","2/2/2007")) 
-
-png("plot1.png",width=640,height=480)
-hist(power_feb$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
+power_feb$DateTime <- strptime(paste(power_feb$Date,power_feb$Time),"%d/%m/%Y %H:%M:%S")
+png("plot2.png",width=640,height=480)
+plot(power_feb$DateTime, power_feb$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=NA)
 dev.off()
-

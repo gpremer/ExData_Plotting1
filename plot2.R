6f983dd4a82b375515efeb38696cd941ea24594d
Geert Premereur <Geert.Premereur@gmail.com>
2015-01-11 18:09:16 +0100

    Added plot 2

---
+++ plot2.R
@@ -0,0 +1,15 @@
+library(dplyr)
+power <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
+power_t <- tbl_df(power)
+
+# POSIXlt not allowed in mutate
+#mutate(power_t,Date2=strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))
+
+
+% power_feb <- filter(power,as.Date(Date,"%d/%m/%Y") %in% c(as.Date(Date,"1/2/2007"),as.Date("2/2/2007","%d/%m/%Y"))) 
+power_feb <- filter(power,Date %in% c("1/2/2007","2/2/2007")) 
+
+png("plot1.png",width=640,height=480)
+hist(power_feb$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
+dev.off()
+

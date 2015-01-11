58e4fc06e2d4a6e815a1b8971f4dcde3c0907fa3
Geert Premereur <Geert.Premereur@gmail.com>
2015-01-11 18:27:23 +0100

    Corrected image dimensions

--- plot1.R
+++ plot1.R
@@ -13,7 +13,7 @@ power_t <- tbl_df(power)
 
 power_feb <- filter(power,Date %in% c("1/2/2007","2/2/2007")) 
 
-png("plot1.png",width=640,height=480)
+png("plot1.png",width=480,height=480)
 hist(power_feb$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
 dev.off()
 
--- plot2.R
+++ plot2.R
@@ -9,6 +9,6 @@ power_t <- tbl_df(power)
 
 power_feb <- filter(power,Date %in% c("1/2/2007","2/2/2007")) 
 power_feb$DateTime <- strptime(paste(power_feb$Date,power_feb$Time),"%d/%m/%Y %H:%M:%S")
-png("plot2.png",width=640,height=480)
+png("plot2.png",width=480,height=480)
 plot(power_feb$DateTime, power_feb$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=NA)
 dev.off()
